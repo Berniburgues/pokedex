6fa1a707101615cc90cef93be8797a20b7bd5feb
Bernardo Burgués <burguesbernardo@gmail.com>
2023-03-29 19:14:22 -0300

    router fixed

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext';
 import './index.css';
-import { router } from './router/Index';
+import { router } from './Router/Index';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
